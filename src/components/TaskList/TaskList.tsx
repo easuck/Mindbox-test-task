15a465ed9220efcb3cdf4da3ca51bff08b16a26c
Егор Асонов <egorasonovvv@gmail.com>
2025-08-30 18:47:10 +0600

    fix adaptive

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import Chip, { ChipType } from '../Chip/Chip';
 import { SetStateAction } from 'react';
 import { clearCompleted } from '../../store/todoSlice';
+import { useMediaQuery } from 'react-responsive';
 
 type Props = {
     children: React.ReactNode;
@@ -14,36 +15,64 @@ type Props = {
 const TaskList = ({children, isFolded, selectedChip, setSelectedChip}: Props) => {
     const todos = useAppSelector(state => state.todos.todos);
     const dispatch = useAppDispatch();
+    const isSmall = useMediaQuery({ maxWidth: 700 });
 
     return (
         <>
             <div className={`TaskList ${isFolded ? 'folded' : ''}`}>
                 {children}
             </div>
-            <div className='ActionBar'>
-                <div>{todos.length} items left</div>
-                <div className='TaskList_chips'>
-                    <Chip name={ChipType.ALL} 
-                        setSelectedChip={setSelectedChip}
-                        selectedChip={selectedChip}
-                    />
-                    <Chip name={ChipType.ACTIVE} 
-                        setSelectedChip={setSelectedChip}
-                        selectedChip={selectedChip}
-                    />
-                    <Chip name={ChipType.COMPLETED} 
-                        setSelectedChip={setSelectedChip}
-                        selectedChip={selectedChip}
-                    />
+            {isSmall && (
+                <div className='ActionBar'>
+                    <div style={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+                        <div>{todos.length} items left</div>
+                        <div style={{cursor: 'pointer'}} 
+                        onClick={() => dispatch(clearCompleted())}
+                        >
+                            Clear completed
+                        </div>
+                    </div>
+                    <div className='TaskList_chips'>
+                        <Chip name={ChipType.ALL} 
+                            setSelectedChip={setSelectedChip}
+                            selectedChip={selectedChip}
+                        />
+                        <Chip name={ChipType.ACTIVE} 
+                            setSelectedChip={setSelectedChip}
+                            selectedChip={selectedChip}
+                        />
+                        <Chip name={ChipType.COMPLETED} 
+                            setSelectedChip={setSelectedChip}
+                            selectedChip={selectedChip}
+                        />
+                    </div>  
                 </div>
-                <div style={{cursor: 'pointer'}} 
-                    onClick={() => dispatch(clearCompleted())}
-                >
-                    Clear completed
+            )}
+            {!isSmall && (
+                <div className='ActionBar'>
+                    <div>{todos.length} items left</div>
+                    <div className='TaskList_chips'>
+                        <Chip name={ChipType.ALL} 
+                            setSelectedChip={setSelectedChip}
+                            selectedChip={selectedChip}
+                        />
+                        <Chip name={ChipType.ACTIVE} 
+                            setSelectedChip={setSelectedChip}
+                            selectedChip={selectedChip}
+                        />
+                        <Chip name={ChipType.COMPLETED} 
+                            setSelectedChip={setSelectedChip}
+                            selectedChip={selectedChip}
+                        />
+                    </div>
+                    <div style={{cursor: 'pointer'}} 
+                        onClick={() => dispatch(clearCompleted())}
+                    >
+                        Clear completed
+                    </div>
                 </div>
-            </div>
-        </>
-        
+            )}
+        </>   
     );
 };
 
